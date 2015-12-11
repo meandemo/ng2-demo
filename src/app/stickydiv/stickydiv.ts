f6d9d6dcfef1fc093f712990627e90fb4b9b6867
Bernard <bernard@ng2goodies.com>
2015-12-11 11:22:45 +0100

    first attempt to implement window resize

--- src/app/stickydiv/stickydiv.ts
+++ src/app/stickydiv/stickydiv.ts
@@ -38,7 +38,7 @@ import {Component, Directive, View, OnInit, AfterViewChecked,
   selector: 'sticky-div',
   template: `
   <!--
-    The 2 div below make the sticky div magic   
+    The 2 div below make the sticky div magic
     We'll comment on the first after.
     The second div includes the user content with ng-content,
     after it has scrolled to it max position,
@@ -49,28 +49,28 @@ import {Component, Directive, View, OnInit, AfterViewChecked,
     When the second div position is changed to 'fixed', it is removed from the
     scrollable content, so we must add a content of the same height in place
     of the the navbar. The first div is doing just this
-  -->     
+  -->
 
   <!-- div 1 -->
 
-    <div *ngIf="is_fixed_ && is_enabled_" 
+    <div *ngIf="is_div2_fixed_"
       [style.height.px]="div_height_"
       [style.width.px]="div_width_"
+      (window:resize)="onResize()"
       style="padding: 0; font-size: 12px; color: black; background-color: red">
       Ouch! If you see this text on the browser, you have a problem with
-      stick div id: {{instance_id_}}. It is likely that  
+      stick div id: {{instance_id_}}. It is likely that
       the maxscroll value is not large enough, please
       increase it.
     </div>
 
   <!-- div 2 -->
 
-    <div [id]="instance_id_" [ngStyle]="setStyles(is_fixed_ && is_enabled_)"
+    <div [id]="div2_id_" [ngStyle]="setStyles(is_div2_fixed_)"
       (window:scroll)="onScroll()">
       <ng-content></ng-content>
     </div>
   `,
-      
   styles: [`
   `],
   directives: [NgIf, NgClass]
@@ -79,98 +79,89 @@ export class StickyDivCmp implements OnInit, AfterViewInit {
   static instance_cnt_ = 0;
   @Input() maxscroll: any;
 
+  public clientHeight: number;
+
+
+  private is_sticky_ = false;
+
   private y_offset_ = 0;
   private div_width_ = 0;
   private div_height_ = 0;
   private div_top_ = 0;
-  private div_current_top_ = 0;
+  private div_initial_top_ = 0;
   private div_left_ = 0;
-  private is_fixed_ = false;
-  private is_enabled_ = false;
   private menu_items_: any[];
-  private instance_id_: string;
+
+  private div2_id_: string;
   private do_check_ = false;
 
+  private is_div2_fixed_ = false;
+  private div2_elm_: any;
 
   //
   // We use the constructor to register a unique id
-  // needed for the div#2 above: <div [id]="instance_id_" ...>
+  // needed for the div#2 above
+  //  <div [id]="div2_id_" ...>
   // and used by getElementById()
   //
   constructor() {
-    this.instance_id_ = `sticky-div-magic-${StickyDivCmp.instance_cnt_}`; 
+    this.div2_id_ = `sticky-div2-magic-${StickyDivCmp.instance_cnt_}`;
     ++StickyDivCmp.instance_cnt_;
   }
 
-  // 
+  //
   // At the ngOnInit() stage, we can't do much
-  // The @Input is not yet injected if the maxscroll value
-  // is not known at this stage.
+  // The @Input is not yet injected as the maxscroll value
+  // is not known at this stage (except if hardcoded value).
   //
   ngOnInit() {
-    if ('maxscroll' in this) {
-      this.is_enabled_ = true;
-    }
-  }
-
-  height(): number {
-    return this.div_height_;
   }
 
   //
   // At the afterViewInit() stage,
   // we can extract the div#2 size and position using getElementById.  
-  // We could not find an 'angular' way to access DOM elements of the template  
-  // Note that without the use of a unique instance_id_,
-  // we could not ensure that getElementById() returns the div#2 object
+  // (could not find an 'angular' way to access DOM elements of the template)  
+  //
+  // Note that the use of a unique id for div#2  (using a static instance counter)
+  // guarantees that getElementById() returns the div#2 
   // linked with 'this', the current class instance.
-  // Note: @Input may not be injected yet.
+  // Note: @Input may not be injected yet!
 
   //
   ngAfterViewInit() {
-    const obj = document.getElementById(this.instance_id_);
-    const bbox = obj.getBoundingClientRect();
-
-    this.div_height_      = bbox.bottom - bbox.top;
-    this.div_width_       = bbox.right - bbox.left;
-    this.div_current_top_ = bbox.top;
+    this.div2_elm_ = document.getElementById(this.div2_id_);
+    this.clientHeight = this.div2_elm_.clientHeight;
+    const bbox = this.div2_elm_.getBoundingClientRect();
+    this.div_height_      = bbox.height;
+    this.div_width_       = bbox.width;
+    this.div_initial_top_ = bbox.top;
     this.div_left_        = bbox.left;
 
-    //console.log('[Trace] afterViewInit()     id     ' + this.instance_id_);
-    //console.log('[Trace] afterViewInit()     top    ' + bbox.top);
-    //console.log('[Trace] afterViewInit()     bottom ' + bbox.bottom);
-    //console.log('[Trace] afterViewInit()     left   ' + bbox.left);
-    //console.log('[Trace] afterViewInit()     right  ' + bbox.right);
+
   }
 
-
-
-  //
   // At the afterViewChecked() stage,
   // @Input is injected, so we can finally obtain the numerical
-  // value associated with this maxscroll property 
+  // value associated with this maxscroll property
   //
   ngAfterViewChecked() {
-    if (!('maxscroll' in this)) {
-      this.is_enabled_ = false;
-      return;
-    }
-    this.is_enabled_ = true;
+    if ('maxscroll' in this) {
 
-    //console.log('[Trace] afterViewChecked() id        = ' + this.instance_id_);
-    this.y_offset_ = 0 + this.maxscroll;  
-    this.div_top_  = this.div_current_top_ - this.y_offset_;   
+      this.is_sticky_ = true;
+      this.y_offset_ = 0 + this.maxscroll;
+      this.div_top_  = this.div_initial_top_ - this.y_offset_;
+    }
   }
 
 
   setStyles(is_fixed: boolean): any {
     if (is_fixed) {
       return {
-        'height': `${this.div_height_}px`,
-        'width': `${this.div_width_}px`,
         'position': 'fixed',
         'padding': '0px',
         'z-index': 140,
+        'height': `${this.div_height_}px`,
+        'width': `${this.div_width_}px`,
         'top': `${this.div_top_}px`,
         'left': `${this.div_left_}px`
       };
@@ -179,22 +170,59 @@ export class StickyDivCmp implements OnInit, AfterViewInit {
     }
   }
 
+
   //
   // When a sticky-div has scrolled up to its
   // max amount, it becomes fixed.
   //
   onScroll() {
-    this.is_fixed_ = (window.pageYOffset >= this.y_offset_);
-
-    // console.log('[Trace] onScroll()         id        = ' + this.instance_id_);
-    // console.log('[Trace] onScroll()         pageYOf.. = ' + window.pageYOffset);
-    // console.log('[Trace] onScroll()         is_fixed  = ' + this.is_fixed_);
+    if (this.is_sticky_) {
+      this.is_div2_fixed_ = (window.pageYOffset >= this.y_offset_);
+    }
   }
+  /*
+      this.div_left_        = bbox.left;
+      this.is_div2_fixed_   = false;
+
+      console.log('[Trace] onScroll()          id     ' + this.instance_id_);
+      console.log('[Trace] onScroll()          offset ' + this.y_offset_);
+      console.log('[Trace] onScroll()          divtop ' + this.div_top_);
+      console.log('[Trace] onScroll()          top    ' + bbox.top);
+      console.log('[Trace] onScroll()          bottom ' + bbox.bottom);
+      console.log('[Trace] onScroll()          left   ' + bbox.left);
+      console.log('[Trace] onScroll()          right  ' + bbox.right);
+
+
+    } else if (window.pageYOffset > this.y_offset_) {
+      this.is_div2_fixed_ = true;
+    } else {
+      this.is_div2_fixed_ = false;
+    }
+  }
+  */
+
 
   //
-  // not yet implemented
+  // Using a non-optiminal page reload feature
   //
   onResize() {
-    //console.log('[Trace] onResize() id        = ' + this.instance_id_);
+    if (this.is_sticky_) {
+      window.location.reload();
+      is_div2_fixed_ = false;
+      this.is_sticky_ = false;
+    }
   }
 }
+
+
+//  const obj = document.getElementById(this.div2_id_);
+//   const bbox = obj.getBoundingClientRect();
+//   this.div_height_      = bbox.height;
+//   this.div_width_       = bbox.width;
+//   this.div_initial_top_ = bbox.top;
+//   this.div_left_        = bbox.left;
+//     this.y_offset_ = 0 + this.maxscroll;
+//     this.div_top_  = this.div_initial_top_ - this.y_offset_;
+//    this.clientHeight = this.div_height_;
+//   }
+
